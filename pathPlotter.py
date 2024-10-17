d6cc84743f45fb9851f2ba65e04f961ba73cddb8
YaBoiPiksure <lordpyrrole@gmail.com>
2024-10-17 16:58:29 -0500

    Additional Plotting Methods

--- pathPlotter.py
+++ pathPlotter.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-if __name__ == "__main__":
 
+def standardPlotter():
     df = pd.read_csv("build/output.csv", index_col=False)
 
     df.drop(df.tail(1).index,inplace=True) 
@@ -21,4 +21,69 @@ if __name__ == "__main__":
         plt.plot(values)
 
     plt.show()
+
+    return
+
+def multiGrayLastFull():
+    df = pd.read_csv("build/output.csv", index_col=False)
+
+    df.drop(df.tail(1).index,inplace=True) 
+
+    print(df.shape)
+
+    print(df.head)
+
+    times = np.arange(df.shape[0])
+
+    
+
+    for column in df.iloc[:,:-1]:
+         values = df[column]
+
+         plt.plot(values, color='gray', alpha=0.5)
+
+    value = df[df.columns[-2]]
+
+    plt.plot(value, color = 'blue', zorder=2)
+
+    plt.show()
+
+    return
+
+def multiGrayAveragePlot():
+    df = pd.read_csv("build/output.csv", index_col=False)
+
+    df.drop(df.tail(1).index,inplace=True) 
+
+
+    averagePath = np.zeros(df.shape[0])
+
+    for index,row in enumerate(df.to_numpy()):
+        row = row[:-1]
+
+        average = np.median(row, axis=0)
+
+        averagePath[index] = average
+
+    plt.plot(averagePath, color = 'red', zorder=3)
+
+    for column in df.iloc[:,:-1]:
+         values = df[column]
+
+         plt.plot(values, color='gray', alpha=0.5)
+
+    value = df[df.columns[-2]]
+
+    plt.plot(value, color = 'blue', zorder=2)
+
+    plt.show()
+
+    return
+
+if __name__ == "__main__":
+    multiGrayLastFull()
+    #standardPlotter()
+    #multiGrayAveragePlot()
+
+    
     
